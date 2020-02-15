e558cbacc17e374e0e11b885ab81884e73231a87
quentin.maloubier <quentin.maloubier@gmail.com>
2020-02-15 17:51:41 +0100

    Troisième itération

--- src/classe/instruction.java
+++ src/classe/instruction.java
@@ -9,29 +9,39 @@ public class instruction {
 	public void SetInstruction(fabricant f) {
 		String commande= f.GetCommande();
 		double montant = f.GetSomme();
+		boolean chaud =false;
 		String[] parts = commande.trim().split(":");
 		String boisson = parts[0];
 		String nbSucre = parts[1];
 		
-		if (boisson == "C") {
+		if (boisson.contains("C") == true) {
 			if(montant >= 0.6) {
-				this.Cafe();
+				if (boisson.contains("h")) {
+					chaud =true;
+				}
+				this.Cafe(chaud);
 			}
 			else {
 				f.ManqueMonnaie(0.6);
 			}
 		}
-		else if (boisson == "T") {
+		else if (boisson.contains("T") == true) {
 			if(montant >= 0.4) {
-				this.The();
+				if (boisson.contains("h")) {
+					chaud =true;
+				}
+				this.The(chaud);
 			}
 			else {
 				f.ManqueMonnaie(0.4);
 			}
 		}
-		else if (boisson == "H") {
+		else if (boisson.contains("H") == true) {
 			if(montant >= 0.5) {
-				this.Chocolat();
+				if (boisson.contains("h")) {
+					chaud =true;
+				}
+				this.Chocolat(chaud);
 			}
 			else {
 				f.ManqueMonnaie(0.5);
@@ -40,6 +50,15 @@ public class instruction {
 		else if (boisson == "M"){
 			this.Message(nbSucre);
 		}
+		else if (boisson == "O") {
+			if(montant >= 0.6) {
+				this.JusOrange();
+			}
+			else {
+				f.ManqueMonnaie(0.6);
+			}
+		}
+		
 		
 		if (nbSucre == "1") {
 			this.AddSucre(1);
@@ -51,20 +70,38 @@ public class instruction {
 		}
 	}
 	
-	public void Cafe() {
-		//code pour faire un café
+	public void Cafe(boolean chaud) {
+		if (chaud == true) {
+			//code pour faire un café très chaud
+		}
+		else {
+			//code pour faire un café
+		}
 	}
 	
-	public void The() {
-		//code pour faire un thé
+	public void The(boolean chaud) {
+		if (chaud == true) {
+			//code pour faire un thé très chaud
+		}
+		else {
+			//code pour faire un thé
+		}
 	}
 	
-	public void Chocolat() {
-		//code pour faire un chocolat
+	public void Chocolat(boolean chaud) {
+		//code pour faire un chocolat 
+		if (chaud == true) {
+			this.Chocolat(false);
+		}
+	}
+	
+	public void JusOrange() {
+		//code pour faire un jus d'orange
 	}
 	
 	public void Message(String message) {
 		//affiche le message
+		System.out.println(message);
 	}
 	
 	public void AddSucre(int nb) {
